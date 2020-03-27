43e104587737d0b94099dc6c6e15acc080a0f1f7
Robson Lima <rsl.robson@gmail.com>
2020-03-27 16:28:54 -0300

    Adicionado arquivo index.js

---
+++ www/omni11/aulas/backend/index.js
@@ -0,0 +1,7 @@
+//importar funcionalidades do express para variável express
+const express = require('express');
+
+const app = express();
+
+//diz para a aplicação ouvir a porta 3333 do localhost
+app.listen(3333);
