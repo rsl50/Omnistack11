4def96c6548e31685a7de11bcd872ca7996a7f18
Robson Lima <rsl.robson@gmail.com>
2020-03-27 16:32:39 -0300

    Adicionado rota e mensagem hello world

--- www/omni11/aulas/backend/index.js
+++ www/omni11/aulas/backend/index.js
@@ -3,5 +3,10 @@ const express = require('express');
 
 const app = express();
 
+//adicionando rota para a aplicação e mensagem hello world
+app.get('/', (request, response)=> {
+    return response.send('Hello World');
+});
+
 //diz para a aplicação ouvir a porta 3333 do localhost
 app.listen(3333);
