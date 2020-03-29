5e506d32c9f4ee62e69235b287399e134474233b
Robson Lima <rsl.robson@gmail.com>
2020-03-29 11:21:50 -0300

    Adicionado rota e método para deletar casos

--- www/omni11/aulas/backend/src/controllers/IncidentController.js
+++ www/omni11/aulas/backend/src/controllers/IncidentController.js
@@ -19,5 +19,29 @@ module.exports = {
         });
         
         return response.json({id});
+    }, 
+
+    async delete(request, response) {        
+        //recebe o id enviado na barra de endereço
+        const { id } = request.params;
+        //pega id da ong logada
+        const ong_id = request.headers.authorization;
+
+        //busca id para confirmar que ong que criou aquele caso
+        const incident = await connection('incidents')
+            .where('id', id)
+            .select('ong_id')
+            .first();
+
+        if (incident.ong_id != ong_id){
+            //retorna erro
+            return response.status(401).json({ error: 'Operation not permitted.' });
+        }
+        
+        //caso contrário, deleta o caso do banco
+        await connection('incidents').where('id', id).delete();
+
+        return response.status(204).send();
     }
+
 };
