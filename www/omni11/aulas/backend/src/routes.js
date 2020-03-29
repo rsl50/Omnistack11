3329e72eb6ba9b21a4b4226846e68e07bc5c6aa3
Robson Lima <rsl.robson@gmail.com>
2020-03-29 11:53:54 -0300

    Adicionado método e rota para ler todos os casos de uma ong

--- www/omni11/aulas/backend/src/routes.js
+++ www/omni11/aulas/backend/src/routes.js
@@ -2,6 +2,7 @@ const express = require('express');
 
 const OngController = require('./controllers/OngController');
 const IncidentController = require('./controllers/IncidentController');
+const ProfileController = require('./controllers/ProfileController');
 
 const routes = express.Router();
 
@@ -11,5 +12,7 @@ routes.post('/ongs', OngController.create);
 routes.post('/incidents', IncidentController.create);
 routes.get('/incidents', IncidentController.index);
 routes.delete('/incidents/:id', IncidentController.delete);
+
+routes.get('/profile/', ProfileController.index);
 
 module.exports = routes;
