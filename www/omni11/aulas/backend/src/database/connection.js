16e2766fe872bef011f060e4fc8f8208f7a6284a
Robson Lima <rsl.robson@gmail.com>
2020-03-28 22:13:16 -0300

    Correção no migration

---
+++ www/omni11/aulas/backend/src/database/connection.js
@@ -0,0 +1,5 @@
+const knex = require('knex');
+const configuration  = require('../../knexfile');
+//conexão de desenvolvimento
+const connection = knex(configuration.development);
+module.exports = connection;
--- www/omni11/aulas/backend/src/routes.js
+++ www/omni11/aulas/backend/src/routes.js
@@ -1,16 +1,23 @@
 const express = require('express');
+const crypto = require('crypto');
+
+const connection = require('./database/connection');
 
 const routes = express.Router();
 
-routes.post('/users/', (request, response)=> {
-    const params = request.body;
-
-    console.log(params);
-
-    return response.json({
-        evento: 'Semana OmniStack 11.0',
-        aluno: 'Robson Souza'
-    });
+routes.post('/ongs', async (request, response)=> {
+    const {name, email, whatsapp, city, uf} = request.body;
+    
+    //gera o id da ong
+    const id = crypto.randomBytes(4).toString('HEX');
+    
+    await connection('ongs').insert({
+        id,
+        name,
+        email,
+        whatsapp,
+        city,
+        uf,
+    })
+    return response.json({ id });
 });
-
-module.exports = routes;
