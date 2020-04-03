8e483d2aacc05284bf3cbff6279b681412c1cfa3
Robson Lima <rsl.robson@gmail.com>
2020-04-03 16:17:47 -0300

    Adicionando campos e ícone na tela de logon

--- www/omni11/aulas/frontend/src/pages/Logon/index.js
+++ www/omni11/aulas/frontend/src/pages/Logon/index.js
@@ -2,12 +2,27 @@ import React from 'react';
 
 import './styles.css';
 import heroesImg from '../../assets/heroes.png';
+import logoImg from '../../assets/logo.svg';
+//Adicionando ícone login
+import { FiLogIn } from 'react-icons/fi';
 
 export default function Logon() {
     return (
         <div className="class-logon-contiainer">
             <section className="form">
+                <img src={logoImg} alt="Be The Hero" />
 
+                <form>
+                    <h1>Faça seu logon</h1>
+
+                    <input placeholder="Sua ID"/>
+                    <button type="submit">Entrar</button>
+                    <a href="/register">
+                        <FiLogIn size={16} color="#e02041" />
+                        Não tenho cadastro
+                    </a>
+                </form>
+            
             </section>
 
             <img src={heroesImg} alt="Heroes" />
