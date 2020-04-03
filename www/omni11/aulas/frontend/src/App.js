2231bfbe407e67f574abc9f5a9078c6c11afff41
Robson Lima <rsl.robson@gmail.com>
2020-04-03 11:16:00 -0300

    Testando Propriedades, Estados e Imutabilidade no React

--- www/omni11/aulas/frontend/src/App.js
+++ www/omni11/aulas/frontend/src/App.js
@@ -1,27 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React, {useState} from 'react';
+import Header from './Header';
 function App() {
+  //estado para armazenar o valor e permitir a atualização na tela
+  const [counter, setCounter] = useState(0);
+  function increment() {
+    setCounter(counter + 1);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-          <p>Hello Omnistack 11</p>
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header>Contador: {counter}</Header>
+      <button onClick={increment}>Incrementar</button>
     </div>
   );
 }
-
 export default App;
