f9259423ea85ba31d00d867ff388db0d2a1a167a
Robson Lima <rsl.robson@gmail.com>
2020-04-03 16:48:30 -0300

    Aplicando estilos na Tela de Logon com css

--- www/omni11/aulas/frontend/src/pages/Logon/index.js
+++ www/omni11/aulas/frontend/src/pages/Logon/index.js
@@ -1,14 +1,15 @@
 import React from 'react';
-
-import './styles.css';
-import heroesImg from '../../assets/heroes.png';
-import logoImg from '../../assets/logo.svg';
 //Adicionando ícone login
 import { FiLogIn } from 'react-icons/fi';
 
+import './styles.css';
+
+import logoImg from '../../assets/logo.svg';
+import heroesImg from '../../assets/heroes.png';
+
 export default function Logon() {
     return (
-        <div className="class-logon-contiainer">
+        <div className="logon-container">
             <section className="form">
                 <img src={logoImg} alt="Be The Hero" />
 
@@ -16,13 +17,13 @@ export default function Logon() {
                     <h1>Faça seu logon</h1>
 
                     <input placeholder="Sua ID"/>
-                    <button type="submit">Entrar</button>
+                    <button className="button" type="submit">Entrar</button>
+
                     <a href="/register">
                         <FiLogIn size={16} color="#e02041" />
                         Não tenho cadastro
                     </a>
-                </form>
-            
+                </form>            
             </section>
 
             <img src={heroesImg} alt="Heroes" />
