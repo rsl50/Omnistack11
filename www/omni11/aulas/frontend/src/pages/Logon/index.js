3a49a19cf03258f702e32fc5818308e354f1d69c
Robson Lima <rsl.robson@gmail.com>
2020-04-05 20:27:26 -0300

    Criando o arquivo index.js para a tela de cadastro

--- www/omni11/aulas/frontend/src/pages/Logon/index.js
+++ www/omni11/aulas/frontend/src/pages/Logon/index.js
@@ -20,7 +20,7 @@ export default function Logon() {
                     <input placeholder="Sua ID"/>
                     <button className="button" type="submit">Entrar</button>
 
-                    <Link to="/register">
+                    <Link className="back-link" to="/register">
                         <FiLogIn size={16} color="#e02041" />
                         Não tenho cadastro
                     </Link>
--- www/omni11/aulas/frontend/src/pages/Register/index.js
+++ www/omni11/aulas/frontend/src/pages/Register/index.js
@@ -1,9 +1,39 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { FiArrowLeft } from 'react-icons/fi';
 
 import './styles.css';
+import logoImg from '../../assets/logo.svg';
 
 export default function Register() {
     return (
-        <h1>Register</h1>
+        <div className="register-container">
+            <div className="content">
+                <section>
+                    <img src={logoImg} alt="Be The Hero" />
+
+                    <h1>Cadastro</h1>
+                    <p>Faça seu cadastro, entre na plataforma e ajude pessoas a encontrarem os casos da sua ONG </p>
+
+                    <Link className="back-link" to="/register">
+                        <FiArrowLeft size={16} color="#e02041" />
+                        Não tenho cadastro
+                    </Link>
+                </section>
+
+                <form>
+                    <input placeholder="Nome da ONG" />
+                    <input input="email" placeholder="E-mail" />
+                    <input placeholder="WhatsApp" />
+
+                    <div className="input-group">
+                        <input placeholder="Cidade" />
+                        <input placeholder="UF" sytle={{ width: 80 }} />
+                    </div>
+
+                    <button className="button" type="submit">Cadastrar</button>
+                </form>
+            </div>
+        </div>
     )
 }
