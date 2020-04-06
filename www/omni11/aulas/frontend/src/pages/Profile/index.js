47bdfdcf38d116211642039ebb0fb6e87b07ee1d
Robson Lima <rsl.robson@gmail.com>
2020-04-06 15:17:53 -0300

    Adicionando a Tela de Profile

--- www/omni11/aulas/frontend/src/pages/Profile/index.js
+++ www/omni11/aulas/frontend/src/pages/Profile/index.js
@@ -1,13 +1,24 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-//Adicionando ícone login
-import { FiLogIn } from 'react-icons/fi';
+import { FiPower } from 'react-icons/fi';
 
 import './styles.css';
 
 import logoImg from '../../assets/logo.svg';
-import heroesImg from '../../assets/heroes.png';
 
 export default function Profile() {
-    return <h1>Profile 1</h1>
+    return (
+        <div className="profile-container">
+            <header>
+                <img src={logoImg} alt="Be The Hero" />
+                <span>Bem vinda, APAD</span>
+
+                <Link className="button" to="/incidents/new">Cadastrar novo caso</Link>
+                <button type="button">
+                    <FiPower size={18} color="#e02041" />
+                </button>
+
+            </header>
+        </div>
+    );
 }
