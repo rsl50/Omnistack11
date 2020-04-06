a7dace8c1934aedf665d9fc20085a9965506de5f
Robson Lima <rsl.robson@gmail.com>
2020-04-06 17:36:37 -0300

    Adicionando a Tela de Incidentes e seu arquivo css

--- www/omni11/aulas/frontend/src/pages/Register/index.js
+++ www/omni11/aulas/frontend/src/pages/Register/index.js
@@ -13,7 +13,7 @@ export default function Register() {
                     <img src={logoImg} alt="Be The Hero" />
 
                     <h1>Cadastro</h1>
-                    <p>Faça seu cadastro, entre na plataforma e ajude pessoas a encontrarem os casos da sua ONG </p>
+                    <p>Faça seu cadastro, entre na plataforma e ajude pessoas a encontrarem os casos da sua ONG.</p>
 
                     <Link className="back-link" to="/">
                         <FiArrowLeft size={16} color="#e02041" />
