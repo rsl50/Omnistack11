cc02f1b9e8429caa77ff63092520612f0af9f14d
Robson Lima <rsl.robson@gmail.com>
2020-04-06 17:07:42 -0300

    Finalizando a estilização da Tela Profile

--- www/omni11/aulas/frontend/src/pages/Profile/index.js
+++ www/omni11/aulas/frontend/src/pages/Profile/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { FiPower } from 'react-icons/fi';
+import { FiPower, FiTrash2 } from 'react-icons/fi';
 
 import './styles.css';
 
@@ -17,8 +17,69 @@ export default function Profile() {
                 <button type="button">
                     <FiPower size={18} color="#e02041" />
                 </button>
-
             </header>
+
+            <h1>Casos cadastrados</h1>
+
+            <ul>
+                <li>
+                    <strong>CASO:</strong>
+                    <p>Caso teste</p>
+
+                    <strong>DESCRIÇÃO:</strong>
+                    <p>Descrição teste</p>
+
+                    <strong>VALOR:</strong>
+                    <p>R$ 120,00</p>
+
+                    <button type="button">
+                        <FiTrash2 size={20} color="#a8a8b3" />
+                    </button>
+                </li>
+                <li>
+                    <strong>CASO:</strong>
+                    <p>Caso teste</p>
+
+                    <strong>DESCRIÇÃO:</strong>
+                    <p>Descrição teste</p>
+
+                    <strong>VALOR:</strong>
+                    <p>R$ 120,00</p>
+
+                    <button type="button">
+                        <FiTrash2 size={20} color="#a8a8b3" />
+                    </button>
+                </li>
+                <li>
+                    <strong>CASO:</strong>
+                    <p>Caso teste</p>
+
+                    <strong>DESCRIÇÃO:</strong>
+                    <p>Descrição teste</p>
+
+                    <strong>VALOR:</strong>
+                    <p>R$ 120,00</p>
+
+                    <button type="button">
+                        <FiTrash2 size={20} color="#a8a8b3" />
+                    </button>
+                </li>
+                <li>
+                    <strong>CASO:</strong>
+                    <p>Caso teste</p>
+
+                    <strong>DESCRIÇÃO:</strong>
+                    <p>Descrição teste</p>
+
+                    <strong>VALOR:</strong>
+                    <p>R$ 120,00</p>
+
+                    <button type="button">
+                        <FiTrash2 size={20} color="#a8a8b3" />
+                    </button>
+                </li>
+            </ul>
+
         </div>
     );
 }
